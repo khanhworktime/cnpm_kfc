f90d8eccebf4a8f8e41b7c98fe00c699a824ff65
chikh <khanhworktime@gmail.com>
2023-02-13 01:25:39 +0700

    Clear old repo

--- server/utils/calcAge.js
+++
@@ -1,7 +0,0 @@
-function calculateAge (birthday) {
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
-module.exports = calculateAge
--- server/utils/omitProp.js
+++
@@ -1,9 +0,0 @@
-function omit(obj, ...props) {
-    const result = { ...obj };
-    props.forEach(function(prop) {
-        delete result[prop];
-    });
-    return result;
-}
-
-module.exports = omit
